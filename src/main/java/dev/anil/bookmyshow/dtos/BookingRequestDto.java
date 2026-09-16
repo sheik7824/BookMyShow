ed34e22f70e862065df2557185c2e565661967f0
sheik7824 <salmansheik7896@gmail.com>
2026-09-16 10:07:56 +0000

    pr for review

--- src/main/java/dev/anil/bookmyshow/dtos/BookingRequestDto.java
+++ src/main/java/dev/anil/bookmyshow/dtos/BookingRequestDto.java
@@ -16,5 +16,5 @@ public class BookingRequestDto {
     private long showId;
     private List<Long> showSeats;
 
-    private void setdemoFileds(String name){};
+    private void setdemoFileds(String namee){};
 }
