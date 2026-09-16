65aaf2c0f5d27deb93d943ef3ac7c791252e550a
sheik7824 <salmansheik7896@gmail.com>
2026-09-16 08:06:48 +0000

    demo changes for pr

--- src/main/java/dev/anil/bookmyshow/dtos/BookingRequestDto.java
+++ src/main/java/dev/anil/bookmyshow/dtos/BookingRequestDto.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class BookingRequestDto {
 
     private long userId;
+    public String DemofieldForPR;
     private long showId;
     private List<Long> showSeats;
 }
